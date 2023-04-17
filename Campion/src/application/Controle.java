7a3aa48e39f6d1c79155f6c945ad78853edb3cd9
Lucas Farias <109489204+FILElucas99@users.noreply.github.com>
2023-04-17 14:10:28 -0300

    Add files via upload
    Interface gráfica de login

---
+++ Campion/src/application/Controle.java
@@ -0,0 +1,12 @@
+package application;
+
+import javafx.event.ActionEvent;
+
+public class Controle {
+	public void entrar(ActionEvent e) {
+		System.out.println("Entrou");
+	}
+	public void cadastrar(ActionEvent e) {
+		System.out.println("Cadastro");
+	}
+}
